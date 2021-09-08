86f75f976f6a97dfee3813732591dfa514846788
Jan Henke <Jan.Henke@taujhe.de>
2021-09-08 20:56:13 +0200

    MVP for mobile

--- mobile/src/main/java/com/github/janhenke/openandnetstat/NetIfAdapter.java
+++ mobile/src/main/java/com/github/janhenke/openandnetstat/NetIfAdapter.java
@@ -1,7 +1,12 @@
 package com.github.janhenke.openandnetstat;
 
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +20,28 @@ public class NetIfAdapter extends ListAdapter<NetworkInterface, NetIfAdapter.Vie
 
 	public static class ViewHolder extends RecyclerView.ViewHolder {
 
-		final TextView textView;
+		final TextView name;
+		final TextView flags;
+		final TextView addresses;
 
 		public ViewHolder(@NonNull final View itemView) {
 			super(itemView);
 
-			textView = itemView.findViewById(R.id.textView);
+			name = itemView.findViewById(R.id.name);
+			flags = itemView.findViewById(R.id.flags);
+			addresses = itemView.findViewById(R.id.addresses);
 		}
 
-		public TextView getTextView() {
-			return textView;
+		public TextView getName() {
+			return name;
+		}
+
+		public TextView getFlags() {
+			return flags;
+		}
+
+		public TextView getAddresses() {
+			return addresses;
 		}
 	}
 
@@ -62,6 +79,31 @@ public class NetIfAdapter extends ListAdapter<NetworkInterface, NetIfAdapter.Vie
 	public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
 		final NetworkInterface networkInterface = getItem(position);
 
-		holder.getTextView().setText(networkInterface.getDisplayName());
+		holder.getName().setText(networkInterface.getDisplayName());
+
+		final List<String> flags = new LinkedList<>();
+		try {
+			if (networkInterface.isUp()) {
+				flags.add("Up");
+			}
+			if (networkInterface.isLoopback()) {
+				flags.add("Loopback");
+			}
+			if (networkInterface.isPointToPoint()) {
+				flags.add("PointToPont");
+			}
+			if (networkInterface.isVirtual()) {
+				flags.add("Virtual");
+			}
+		} catch (final SocketException e) {
+			Log.e(NetIfAdapter.class.getSimpleName(), "Getting properties of network interface failed.", e);
+		}
+		holder.getFlags().setText(String.join(" ", flags));
+
+		final String addresses = networkInterface.getInterfaceAddresses()
+												 .stream()
+												 .map(i -> i.getAddress() + "/" + i.getNetworkPrefixLength())
+												 .collect(Collectors.joining("\n"));
+		holder.getAddresses().setText(addresses);
 	}
 }
